2152c2ecd9d81c5843d1a385f1da7e763e18b337
Viktor Fonic <viktor.fonic@gmail.com>
2020-01-03 19:11:47 +0300

    Allow adding tasks directly to the calendar

--- index.js
+++ index.js
@@ -1,39 +1,64 @@
-document.addEventListener('DOMContentLoaded', function() {
-  var Calendar = FullCalendar.Calendar;
-  var Draggable = FullCalendarInteraction.Draggable
+var Calendar = FullCalendar.Calendar;
+var Draggable = FullCalendarInteraction.Draggable
 
-  var containerEl = document.getElementById('external-events-list');
-  new Draggable(containerEl, {
-    itemSelector: '.fc-event',
-    eventData: function(eventEl) {
-      return {
-        title: eventEl.innerText.trim()
-      }
+var containerEl = document.getElementById('external-events-list');
+new Draggable(containerEl, {
+  itemSelector: '.fc-event',
+  eventData: function(eventEl) {
+    return {
+      title: eventEl.innerText.trim()
     }
-  });
-
-  var calendarEl = document.getElementById('calendar');
-  var calendar = new Calendar(calendarEl, {
-    plugins: [ 'bootstrap', 'interaction', 'dayGrid', 'timeGrid', 'list' ],
-    themeSystem: 'bootstrap',
-    header: {
-      left: 'prevYear,prev,next,nextYear today',
-      center: 'title',
-      right: 'dayGridMonth,timeGridWeek,timeGridDay,listWeek'
-    },
-    // defaultDate: '2019-08-12',
-    weekNumbers: false,
-    navLinks: true, // can click day/week names to navigate views
-    droppable: true, // this allows things to be dropped onto the calendar
-    drop: function(arg) {
-      // is the "remove after drop" checkbox checked?
-      // if (document.getElementById('drop-remove').checked) {
-      //   // if so, remove the element from the "Draggable Events" list
-      //   arg.draggedEl.parentNode.removeChild(arg.draggedEl);
-      // }
-    },
-    editable: true,
-    eventLimit: false, // allow "more" link when too many events
-  });
-  calendar.render();
+  }
 });
+
+var calendarEl = document.getElementById('calendar');
+var calendar = new Calendar(calendarEl, {
+  plugins: [ 'bootstrap', 'interaction', 'dayGrid', 'timeGrid', 'list' ],
+  themeSystem: 'bootstrap',
+  header: {
+    left: 'prevYear,prev,next,nextYear today',
+    center: 'title',
+    right: 'dayGridMonth,timeGridWeek,timeGridDay,listDay,listWeek'
+  },
+  // customize the button names,
+  // otherwise they'd all just say "list"
+  views: {
+    listDay: { buttonText: 'list day' },
+    listWeek: { buttonText: 'list week' }
+  },
+  defaultView: 'listDay',
+  weekNumbers: false,
+  navLinks: true, // can click day/week names to navigate views
+  droppable: true, // this allows things to be dropped onto the calendar
+  drop: function(arg) {
+    // is the "remove after drop" checkbox checked?
+    // if (document.getElementById('drop-remove').checked) {
+    //   // if so, remove the element from the "Draggable Events" list
+    //   arg.draggedEl.parentNode.removeChild(arg.draggedEl);
+    // }
+  },
+  editable: true,
+  eventLimit: false, // allow "more" link when too many events
+  selectable: true,
+  selectMirror: true,
+  select: function(arg) {
+    var title = prompt('Task:');
+    if (title) {
+      calendar.addEvent({
+        title: title,
+        start: arg.start,
+        end: arg.end,
+        allDay: arg.allDay
+      })
+    }
+    calendar.unselect()
+  },
+});
+calendar.render();
+
+// calendar.addEvent({
+//   title: title,
+//   start: arg.start,
+//   end: arg.end,
+//   allDay: arg.allDay
+// })
